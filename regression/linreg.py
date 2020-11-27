a89c84c5d9f0e4f2e3f5206d898d178a1ad3af5f
Atharva Kench <atharva.kench@gmail.com>
2020-11-26 19:16:35 -0500

    generalize linreg and graph loss gradient

--- regression/linreg.py
+++ regression/linreg.py
@@ -2,100 +2,117 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def gen_data(num_samples):
+    # b, w1
+    w = np.array([3, -1])
+
     data = []
     for _ in range(num_samples):
-        x = random.uniform(-10, 10)
-        y = fn(x)
+        x = np.array([1, random.uniform(-10, 10)])
+        y = np.dot(w, x)
 
-        data.append((x, y + random.gauss(0, 0)))
+        data.append((x, y + random.gauss(0, 2)))
     return data
 
 
 def plot_data(data, w, name):
     plt.clf()
 
-    plt.xlim(-12, 12)
-    plt.ylim(-20, 25)
-
+    # plot regression line
     x = np.linspace(-10, 10, 2)
-    y = model(w, x)
+    xvector = np.array([ [1, n] for n in x ])
+
+    y = np.dot(xvector, w)
     plt.plot(x, y, '-r', label='model')
     
-
+    # plot data points
     xs = []
     ys = []
     for x,y in data:
-        xs.append(x)
+        xs.append(x[1])
         ys.append(y)
     plt.scatter(xs, ys)
     plt.grid()
-    plt.savefig(name + '.jpg')
+    plt.savefig('out/' + name + '.jpg')
 
 
-def fn(x):
-    return 2*x + 5
+def plot_loss_gradient(data):
+    plt.clf()
+
+    size = 30
+    x = np.linspace(-10, 10, size)
+    y = np.linspace(-10, 10, size)
+
+    X, Y = np.meshgrid(x, y)
 
 
-def model(w, x):
-    b, m = w
-    return b + m*x
+    Z = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(mse_loss(data, np.array([X[i, j], Y[i, j]])))
+        Z.append(row)
+    Z = np.array(Z)
 
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.contour3D(X, Y, Z, 50)
+    ax.set_xlabel('bias')
+    ax.set_ylabel('slope')
+    ax.set_zlabel('error')
+    fig.savefig('error_gradient.jpg')
+
+    min_index = np.unravel_index(Z.argmin(), Z.shape)
+    print('error of', Z[min_index], 'at coords: (', X[min_index], Y[min_index], ')')
 
 def train(data, alpha):
-    w = [random.uniform(-1, 1), random.uniform(-1, 1)]
-    print(w)
-
-    for iter in range(10000):
-
+    w = np.array([0,0])
+    for iter in range(7001):
         if iter % 1000 == 0:
             plot_data(data, w, str(iter))
-
-        loss_gradient = sum_loss_gradients(data, w)
-        # x, y = random.sample(data, 1)[0]
-        # loss_gradient = get_loss_gradient(x, y, w)
-
-        new_w = []
-        for i in range(len(w)):
-            new_w.append(w[i] - alpha*loss_gradient[i])
-        w = new_w
-
-        if iter % 10 == 0:
-            print('loss gradient', loss_gradient)
-            print('new weights', w)
+            print('iter:', iter)
+            print('weights:', list(w))
+            print('MSE:', mse_loss(data, w))
             print()
+
+        # loss_gradient = sum_loss_gradients(data, w)
+        x, y = random.sample(data, 1)[0]
+        loss_gradient = get_loss_gradient(x, y, w)
+        w = w - alpha*loss_gradient
 
     return w
 
 
 def sum_loss_gradients(data, w):
     gradients = [get_loss_gradient(x, y, w) for x, y in data]
-    return list(map(sum, zip(*gradients)))
+    return sum(gradients)
 
 def get_loss_gradient(x, y, w):
     # loss = (model(w,x) - y) ** 2
     # dloss/dw =  2(model(w,x) - y) * x
     # gradient of loss wrt. all weights =  2(model(w,x) - y) * xvector
     #                                       scalar      *      vector    ==> vector gradient
-    xvector = [1, x]
-    scalar = 2 * (model(w,x) - y)
-    
-    test = [scalar * d for d in xvector]
-    return test
+    scalar = 2 * (np.dot(w, x) - y)
+    return scalar * x
+
 
 def mse_loss(data, w):
     sum = 0
     for x, y in data:
-        loss = (model(w, x) - y) ** 2
+        loss = (np.dot(w, x) - y) ** 2
         sum += loss
     return sum / len(data)
+
 
 if __name__ == '__main__':
     """
     given: some data points
     """
     data = gen_data(100)
-    w = train(data, 0.00001)
+    plot_loss_gradient(data)
+
+    w = train(data, 0.001)
     plot_data(data, w, 'final')
     print('total loss', mse_loss(data, w))
 
